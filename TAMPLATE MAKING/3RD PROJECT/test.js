9f4d656d51f4747ae9686eaa222ea319a9e349c9
muhammadbasit01 <119048499+muhammadbasit01@users.noreply.github.com>
2022-12-07 15:22:54 +0500

    Add files via upload

---
+++ TAMPLATE MAKING/3RD PROJECT/test.js
@@ -0,0 +1,93 @@
+// var marks = prompt("enter your percentage: ");
+
+// if( marks >= 80 && marks <=100 ){
+//     alert("you got 1st division");
+// }else if( marks >= 70 && marks < 80 ){
+//     alert("you got 2nd division");
+// }else if( marks >= 60 && marks < 70 ){
+//     alert("you got 3rd division");
+// }else if(marks >= 50 && marks < 60 ){
+//     alert("you are just pass");
+// }else if( marks >= 0 && marks < 50 ){
+//     alert("you are fail");
+// }else{
+//     alert("the number you dial is not responding at the moment");
+// }
+// // var a = 21;
+// // var  b;
+// // (a >= 18)?  b = "true" : b = "false";
+// // document.write(b);
+// //   var day = 3;
+   
+// //   switch  (day) {
+// //     case 0:
+// //     document.write("today is monday");
+// //     break;
+// //     case 1:
+// //     document.write("today is tuesday");
+// //     break;
+// //     case 2:
+// //     document.write("today is wedsday");
+// //     break;
+// //     case 3:
+// //     document.write("today is thursday");
+// //     break;
+// //     case 4:
+// //     document.write("today is friday");
+// //     break;
+// //     case 5:
+// //     document.write("today is saturday");
+// //     break;
+// //     case 6:
+// //     document.write("today is sunday");
+// //     break;
+// //     default:
+// //     document.write(" enter the valid date")
+// //   }
+
+
+// // var a = confirm ("are you a muslim?");
+
+// // if(a){
+// //     alert("thanks");
+// // }else{
+// //     alert("sorry");
+// // }
+// function khan(){
+//     confirm("yes");
+// }
+
+// khan();
+// khan();
+// khan();
+// khan();
+// khan();
+// khan();
+
+// khan();
+// khan();
+// khan();
+
+// khan();
+
+// do loop
+// var a = 1;
+//  do{
+//     document.write( a + ")   hello khan  <br> ");
+//     a = a + 1
+//  }while(  a <=50 )
+
+// // nested loop
+// for (var a = 1;  a <= 100 ; a++ ){
+//     for( var b = 1; b <= a ; b++ ){
+
+//         document.write( a + "  ")
+//     }
+//     document.write(  "<br>  ")
+// }
+
+// Array
+  var ary =[ 10, 20 , 30 , 40, 60];
+    
+  document.write(ary);
+  
